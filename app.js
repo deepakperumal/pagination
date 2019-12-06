e2df019c6121d574d7d24685bcf25e79f5c6080c
akshay <mehtaakshay22@gmail.com>
2019-12-06 16:15:17 +0530

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['pgModule']);
+
+/*Controller*/
 
 app.controller('tableController', [
   '$scope',
   'getResult',
   function($scope, getResult) {
     $scope.page = 1; //Page Number Declaration
-    $scope.heading = ['Id', 'Title'];
+    $scope.heading = ['Id', 'Title', 'Body'];
     // Watch Page
     $scope.$watch('page', function() {
       beginGetData();
@@ -40,6 +42,8 @@ app.controller('tableController1', [
   }
 ]);
 
+/*Service*/
+
 app.service('getResult', [
   '$http',
   '$q',
@@ -59,52 +63,3 @@ app.service('getResult', [
     };
   }
 ]);
-
-/*Pagination Directive*/
-
-app.directive('ngPaginate', function() {
-  return {
-    restrict: 'AE',
-    transclude: true,
-    scope: {
-      data: '=',
-      page: '=',
-      limit: '=',
-      paginationType: '@',
-      paginationHeading: '=',
-      itemLimit: '@',
-      removeItem: '='
-    },
-    controller: ['$scope', function pgController($scope) {}],
-    link: function(scope, element, attribute) {
-
-
-      scope.pageLimit = [];
-      scope.tableHead = {};
-
-      if (scope.paginationType == 2) {
-        for (var i = scope.limit[0]; i <= scope.limit[1]; i++)
-          scope.pageLimit.push(i);
-      }
-
-      scope.changePage = function(flag) {
-        var limit = scope.limit;
-        let tempPage = scope.page;
-
-        if (flag == 1) tempPage++;
-        else tempPage--;
-
-        if (tempPage >= limit[0] && tempPage <= limit[1]) scope.page = tempPage;
-      };
-
-      scope.ch = function(count) {
-        scope.page = count;
-      };
-
-      scope.checkExist = function(key) {
-        return scope.removeItem.indexOf(key) >= 0;
-      };
-    },
-    templateUrl: './paginate.html'
-  };
-});
