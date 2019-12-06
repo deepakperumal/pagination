bfc2723d09212fa07573bf1f4d82096e6cc92bab
akshay <mehtaakshay22@gmail.com>
2019-12-06 13:23:27 +0530

    first commit

--- app.js
+++ app.js
@@ -4,25 +4,39 @@ app.controller('tableController', [
   '$scope',
   'getResult',
   function($scope, getResult) {
-    $scope.page = 1;
-    $scope.result = {};
-    let url = 'https://jsonplaceholder.typicode.com/posts?userId=';
+    $scope.page = 1; //Page Number Declaration
+    $scope.heading = ['Id', 'Title'];
+    // Watch Page
+    $scope.$watch('page', function() {
+      beginGetData();
+    });
 
+    let url = 'https://jsonplaceholder.typicode.com/posts?userId=';
     let beginGetData = () => {
       getResult.getResult(url + $scope.page).then(function(response) {
- 
-        if(response.length!=0)
         $scope.result = response;
-        else
-        alert('Data not Found')
       });
     };
-    beginGetData();
-    $scope.$watch('page', function(newValue, oldValue) {
-      if (newValue !== oldValue) {
-        beginGetData();
-      }
+  }
+]);
+
+app.controller('tableController1', [
+  '$scope',
+  'getResult',
+  function($scope, getResult) {
+    $scope.page = 1; //Page Number Declaration
+    $scope.heading = ['Id', 'Title'];
+    // Watch Page
+    $scope.$watch('page', function() {
+      beginGetData();
     });
+
+    let url = 'https://jsonplaceholder.typicode.com/posts?userId=';
+    let beginGetData = () => {
+      getResult.getResult(url + $scope.page).then(function(response) {
+        $scope.result = response;
+      });
+    };
   }
 ]);
 
@@ -46,15 +60,49 @@ app.service('getResult', [
   }
 ]);
 
+/*Pagination Directive*/
+
 app.directive('ngPaginate', function() {
   return {
     restrict: 'AE',
     transclude: true,
     scope: {
       data: '=',
-      page: '='
+      page: '=',
+      limit: '=',
+      paginationType: '@',
+      paginationHeading: '=',
+      itemLimit: '@',
+      removeItem: '='
     },
     controller: ['$scope', function pgController($scope) {}],
+    link: function(scope, element, attribute) {
+      scope.pageLimit = [];
+      scope.tableHead = {};
+
+      if (scope.paginationType == 2) {
+        for (var i = scope.limit[0]; i <= scope.limit[1]; i++)
+          scope.pageLimit.push(i);
+      }
+
+      scope.changePage = function(flag) {
+        var limit = scope.limit;
+        let tempPage = scope.page;
+
+        if (flag == 1) tempPage++;
+        else tempPage--;
+
+        if (tempPage >= limit[0] && tempPage <= limit[1]) scope.page = tempPage;
+      };
+
+      scope.ch = function(count) {
+        scope.page = count;
+      };
+
+      scope.checkExist = function(key) {
+        return scope.removeItem.indexOf(key) >= 0;
+      };
+    },
     templateUrl: './paginate.html'
   };
 });
