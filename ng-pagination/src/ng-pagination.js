c3525a41a191d8eda6a7c9f5ae0e6977183d38e7
akshay <mehtaakshay22@gmail.com>
2019-12-09 16:09:48 +0530

    first commit

--- ng-pagination/src/ng-pagination.js
+++ ng-pagination/src/ng-pagination.js
@@ -13,16 +13,24 @@ pgModule.directive('ngPaginate', function() {
       paginationType: '@',
       paginationHeading: '=',
       itemLimit: '@',
-      removeItem: '='
+      showItem: '='
     },
     controller: ['$scope', function pgController($scope) {}],
     link: function(scope, element, attribute) {
       scope.pageLimit = [];
       scope.tableHead = {};
 
+      let generateLimit = startLimit => {
+        let temp = [];
+
+        for (i = startLimit; i <= 10 + startLimit; i++)
+          if (i >= scope.limit[0] && i <= scope.limit[1]) temp.push(i);
+
+        return temp;
+      };
+
       if (scope.paginationType == 2) {
-        for (var i = scope.limit[0]; i <= scope.limit[1]; i++)
-          scope.pageLimit.push(i);
+        scope.pageLimit = generateLimit(0);
       }
 
       scope.changePage = function(flag) {
@@ -33,6 +41,11 @@ pgModule.directive('ngPaginate', function() {
         else tempPage--;
 
         if (tempPage >= limit[0] && tempPage <= limit[1]) scope.page = tempPage;
+        if (scope.page >= 10) {
+          scope.pageLimit = generateLimit(scope.page);
+        } else if (scope.page < 10) {
+          scope.pageLimit = generateLimit(0);
+        }
       };
 
       scope.ch = function(count) {
@@ -40,9 +53,13 @@ pgModule.directive('ngPaginate', function() {
       };
 
       scope.checkExist = function(key) {
-        return scope.removeItem.indexOf(key) >= 0;
+        return scope.showItem.indexOf(key) >= 0;
+      };
+
+      scope.toggleClass = function(val) {
+        return val === scope.page;
       };
     },
-    templateUrl: 'ng-pagination/partials/template/pg-table.html'
+    templateUrl: './ng-pagination/partials/template/pg-table.html'
   };
 });
